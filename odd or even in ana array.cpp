89dcee66912001042dad1c4315b50d94d7c9509a
Dhasvanthini <129880780+Dhasvanthini@users.noreply.github.com>
2023-08-05 13:29:45 +0530

    Add files via upload

---
+++ odd or even in ana array.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the number:");
+	scanf("%d",&n);
+	int arr[n];
+ printf("enter the %d elements in an array");
+	for( i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+}
+	printf("the even numbers in an array are:");
+    {
+	
+    if(arr[i]%2==0)
+	for(int i=0;i<n;i++)
+    printf("%d",arr[i]);
+}
+	    printf("the odd numbers in an array:");
+	 {
+	 
+	 if(arr[i]%2==1)
+		for(int i=0;i<n;i++)
+	printf("%d",arr[i]);
+}
+    
+}
